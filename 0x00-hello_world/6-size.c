d3d5a936d2125743034d5ee19753b48b99ec58d4
Daniel John <dj71286@gmail.com>
2022-10-13 05:28:13 -0700

    Printing the sizes of types1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,17 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+{
+	int i;
+	char c;
+	long int t;
+	long long int o;
+	float f;
+
 	printf("Size of a char:", sizeof(char));
 	printf("Size of an int:", sizeof(int));
 	printf("Size of a long int:", sizeof(long int));
 	printf("Size of a long long int:", sizeof(long long int));
-	printf("Size of a float:", sizeof(float);
+	printf("Size of a float:", sizeof(float));
 	return (0);
 }
