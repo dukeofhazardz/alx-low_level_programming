e01f6929f7dc013e29736865e7349c343ff8c0ca
Daniel John <dj71286@gmail.com>
2022-10-14 09:09:26 -0700

    Printing lower and upper Alphabetical characters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * Description: 'the program's description'
+ * Followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char let;
+
+	for (let = 'a' ; let <= 'z' ; let++)
+
+		putchar(let);
+	for (let = 'A' ; let <= 'Z' ; let++)
+		putchar(let);
+	putchar('\n');
+
+	return (0);
+}
+
