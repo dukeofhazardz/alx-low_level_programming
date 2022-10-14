04d6dcbc2101852a3ce356cdb75c898732555c97
Daniel John <dj71286@gmail.com>
2022-10-14 09:41:21 -0700

    Printing numbers 0 to 10 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * Description: 'the program's description'
+ * Followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num++)
+
+		putchar(num);
+
+	putchar('\n');
+
+	return (0);
+}
+
