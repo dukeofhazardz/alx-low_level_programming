e95cb680fd56fd5afc696b85f6d5ac644f1c1c7c
Daniel John <dj71286@gmail.com>
2022-10-14 09:53:54 -0700

    Printing numbers 0 to 10 with putchar1

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,17 +2,17 @@
 /**
  * main - prints all single digit numbers of base 10 starting from 0,
  * Description: 'the program's description'
- * Followed by a new line
+ * Followed by a new line, using putchar
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int num;
 
-	for (num = 67 ; num < 77 ; num++)
-
+	for (num = 48 ; num < 58 ; num++)
+	{
 		putchar(num);
-
+	}
 	putchar('\n');
 
 	return (0);
