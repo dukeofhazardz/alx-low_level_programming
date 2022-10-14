4a955d8aaa55d52d328fb4039dbe46df5c70a515
Daniel John <dj71286@gmail.com>
2022-10-14 09:30:05 -0700

    Printing numbers 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * Description: 'the program's description'
+ * Followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (num = 0 ; num <= 10 ; num++)
+	{
+		printf("%d\n", num);
+	}
+
+	return (0);
+}
+
