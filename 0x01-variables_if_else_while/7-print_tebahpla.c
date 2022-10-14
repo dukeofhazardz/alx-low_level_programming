bb3ac213f0de0254e07c69ca4f667b75d5e438c9
Daniel John <dj71286@gmail.com>
2022-10-14 10:01:39 -0700

    Printing lower alphabets in reverse using putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse,
+ * Description: 'the program's description'
+ * Followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char let;
+
+	for (let = 'z' ; let <= 'a' ; let--)
+	{
+		putchar(let);
+	}
+	printf("\n");
+
+	return (0);
+}
+
