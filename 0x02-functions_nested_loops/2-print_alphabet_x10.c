25df02549c0a0ac891f28229d1d7927c4dca7ded
Daniel John <dj71286@gmail.com>
2022-10-18 07:34:55 -0700

    Printing a-z ten times in a nested loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints a-z ten times
+ *
+ * Return: Always 0.
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('\n');
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+	}
+}
