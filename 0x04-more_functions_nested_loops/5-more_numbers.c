dfd500659e18dbebdb600582b26fb1257c447ac0
Daniel John <dj71286@gmail.com>
2022-10-20 16:38:15 -0700

    Printing 1 to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,11 @@ void more_numbers(void)
 
 	for (l = 0; l < 10; l++)
 	{
-		for (n = 0; n < 15; n++)
+		for (m = 0; n < 15; m++)
 		{
-			if (n >= 10)
-				_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
+			if (m >= 10)
+				_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 		}
 		_putchar('\n');
 	}
