bdf7467d20323fbb52606bed3840dda7f982572b
Daniel John <dj71286@gmail.com>
2022-10-20 17:54:47 -0700

    Printing the right angled triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	char ch = '#';
+	int a, b, c;
+
+	for (a = 1, a <= size; a++)
+	{
+		for (b = 1; b <= (size - a); b++)
+		{
+			_putchar(' ');
+		}
+		for (c = 1; c <= a; c++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
