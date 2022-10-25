fcd47782b6f2fd17bb7eb4840599bd721056f04f
Daniel John <dj71286@gmail.com>
2022-10-24 17:28:03 -0700

    Even Numbers in a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,15 +8,17 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i, len;
 
-	while (str[i] != 0)
+	len = 0
+
+	while (str[len] != 0)
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-			i++;
-		}
+		len++;
+	}
+	for (i = 0; i < len; i += 2)
+	{
+		_puchar(str[i]);
 	}
 	_putchar('\n');
 }
