be0c92b1453065d7abbd52355a33a6a71116c893
Daniel John <dj71286@gmail.com>
2022-10-24 17:49:10 -0700

    Printing an array of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - Prints n elements of an array of integers,
+ * followed by a new line.
+ * @a: is an arguement
+ * @n: is an arguement
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n, i++)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n");
+}
