0e1ab976604f6c56a55ec86dc4e5d66267605cb9
Daniel John <dj71286@gmail.com>
2022-10-24 17:18:46 -0700

    Even Numbers in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - rints every other character of a string,
+ * starting with the first character.
+ * @str: is a string arguement.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i];
+			i++;
+	}
+	_putchar('\n');
+}
+
