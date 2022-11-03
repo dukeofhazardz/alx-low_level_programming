a5600f12f3c536e177fba32e3c58a272f3b45f0c
Daniel John <dj71286@gmail.com>
2022-11-02 17:26:13 -0700

    Raising to power recursively

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - A function that returns the value
+ * of x raised to the power of y.
+ * @x: number to be raised
+ * @y: exponent
+ * Return: 0, -1 and recursive function
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
