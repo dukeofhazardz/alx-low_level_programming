436e73e256f669b7d37808d1b8f25459e4595bd0
Daniel John <dj71286@gmail.com>
2022-11-04 12:01:03 +0000

    Printing a program's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+
+/**
+ * main - The main function.
+ * @argc: The arguement counter.
+ * @argv: The arguement vector
+ */
+
+int main(int argc, char *argv[])
+{
+	UNUSED(argc);
+	printf("%s\n", argv[0]);
+	return (0);
+}
