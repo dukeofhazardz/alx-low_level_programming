b44f58f2d9538f6f9f738defe46a8b1ec8886826
Daniel John <dj71286@gmail.com>
2022-11-04 13:15:18 +0000

    Print the number of arguements

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - The main function.
@@ -8,17 +9,8 @@
  * Return: 0 Always.
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	int i, sum;
-
-	if (i >= 0)
-	{
-		for (i = 0; i < argc; i++)
-		{
-			sum += atoi(argv[i]);
-		}
-		printf("%d\n", sum);
-	}
+	printf("%d\n", argc - 1);
 	return (0);
 }
