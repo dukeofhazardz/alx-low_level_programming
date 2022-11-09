1dcca2b29d43f81ae61e3f4c9cb8bb482e99ba4b
Daniel John <dj71286@gmail.com>
2022-11-09 08:38:24 -0800

    Printing arguements to main

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - A function that concatenates all the
+ * arguments of your program.
+ * @ac: length of string
+ * @av: string arguement
+ * Return: pointer
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *ptr;
+	int i, j, k, size;
+
+	if (ac == 0 || *av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			size++;
+		}
+	}
+	size += (ac + 1);
+	ptr = malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	k = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			ptr[k] = av[i][j];
+			k++;
+		}
+		ptr[k] = '\n';
+	}
+	ptr[k] = '\0';
+
+	return (ptr);
+}
