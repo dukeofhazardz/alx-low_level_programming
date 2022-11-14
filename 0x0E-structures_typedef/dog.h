4d2aa4b28cae90646924713b18aa27012253ca97
Daniel John <dj71286@gmail.com>
2022-11-14 17:20:16 +0000

    Creating dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * File - dog.h
+ * struc dog - structure of dog
+ * @name: name of dog
+ * @age: age of dog
+ * owner: owner of dog
+ * Author - Daniel John
+ * Description: Header file contains elements for struct dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
