eae9595673ce7320022a44182205851d92fa1c36
Daniel John <dj71286@gmail.com>
2022-11-17 20:27:02 +0000

    Adding integers using Variadic functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,15 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+/**
+ * File - variadic_functions.h
+ * Author - Daniel John
+ * Description: Header contains prototype for all functions
+ * 		used in 0x10. C - Variadic functions.
+ */
+
+int sum_them_all(const unsigned int n, ...);
+
+
+
+#endif
