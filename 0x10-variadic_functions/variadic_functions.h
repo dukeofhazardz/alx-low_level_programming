d3878c7b2a063c333568f89c221e37750ff72c1f
Daniel John <dj71286@gmail.com>
2022-11-17 21:58:34 +0000

    Printing anything with Variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,9 +8,26 @@
  * 		used in 0x10. C - Variadic functions.
  */
 
+#include <stdarg.h>
+
+/**
+ * struct print - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ * 		a data type corresponding to symbol.
+ */
+typedef struct print
+{
+
+	char *symbol;
+	void (*print)(va_list arg);
+
+} print_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 
 
