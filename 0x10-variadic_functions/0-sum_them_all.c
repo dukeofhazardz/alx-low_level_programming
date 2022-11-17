13c50f1a275c4650bb7dc38f1fc085d4405cdcb2
Daniel John <dj71286@gmail.com>
2022-11-17 20:54:27 +0000

    Printing numbers using Variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,13 +15,15 @@ int sum_them_all(const unsigned int n, ...)
 
 	sum = 0;
 
+	if (n == 0)
+	{
+		return (0);
+	}
+
 	va_start(ptr, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (n == 0)
-			return (0);
-
 		sum += va_arg(ptr, int);
 	}
 	va_end(ptr);
