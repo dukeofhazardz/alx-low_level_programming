3d6834b1fbf0931f37f35351f3c93f0da95699cc
Daniel John <dj71286@gmail.com>
2022-11-24 16:15:56 -0800

    Free list commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,7 +5,7 @@
  * Author - Daniel John
  * File - list.h
  * Description: A header file for function prototypes in project
- * 		0x12. C - Singly linked lists
+ *		0x12. C - Singly linked lists
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -31,6 +31,7 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 
 #endif
