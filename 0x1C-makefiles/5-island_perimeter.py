852a76eb5b099cb81904bc17418ae7a420c49a06
Daniel John <dj71286@gmail.com>
2023-03-22 14:30:28 +0000

    Bug fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A function that returns the perimeter of the island described in grid"""
 
+
 def island_perimeter(grid):
     """Return the perimiter of an island.
     The grid represents water by 0 and land by 1.
