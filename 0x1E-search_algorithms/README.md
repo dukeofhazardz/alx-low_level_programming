7b3221ffc57e21898b0c0c998efb17e6d161cf32
Daniel John <dj71286@gmail.com>
2023-05-08 22:20:14 +0100

    finished mandatory search algorithms

---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+A readme on search algorithms in c low level programming
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+
+/*
+ * Author: Nnaemeka Daniel John
+ * File: Header file
+ * Desc: A header file for search algorithms
+ */
+
+/* Prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
