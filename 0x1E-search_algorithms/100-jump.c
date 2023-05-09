39f63911bb1d004f7fe973239703024236a9cd75
Daniel John <dj71286@gmail.com>
2023-05-09 21:52:30 +0100

    fix at 100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -30,7 +30,13 @@ int jump_search(int *array, size_t size, int value)
 	for (; i < jump && array[i] < value; i++)
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
-	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	/* printf("Value checked array[%ld] = [%d]\n", i, array[i]); */
 
-	return (array[i] == value ? (int)i : -1);
+	/* return (array[i] == value ? (int)i : -1); */
+	if (array[i] == value)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		return (i);
+	}
+	return (-1);
 }
